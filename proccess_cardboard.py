0a77e5ba2593992112355ac4b28d79c6af934e56
Jesse Wynn <jesse_wynn@byu.edu>
2017-04-10 10:17:48 -0600

    added blob detector

--- proccess_cardboard.py
+++ proccess_cardboard.py
@@ -4,6 +4,23 @@ import numpy as np
 #create VideoCapture object
 cap = cv2.VideoCapture("test1.avi")
 
+#create blob detector parameter object
+params = cv2.SimpleBlobDetector_Params()
+#filter by size
+params.filterByArea = True
+params.minArea = 100
+#filter by circularity
+params.filterByCircularity = True
+params.minCircularity = 0.5
+#filter by convexity
+params.filterByConvexity = True
+params.minConvexity = 0.87
+#filter by inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.35
+#create blob detector with params object
+blob_detect = cv2.SimpleBlobDetector(params)
+
 while cap.isOpened():
     #get the current frame
     ret, frame = cap.read()
@@ -38,13 +55,20 @@ while cap.isOpened():
     #threshold
     _, gray_thresh = cv2.threshold(gray, 230, 255, 1)
     #erode to kill the noise
-    kernel = np.zeros((11,11),np.uint8)
-    gray_erode = cv2.erode(gray_thresh,kernel,iterations = 1)
+    #kernel = np.zeros((11,11),np.uint8)
+    #gray_erode = cv2.erode(gray_thresh,kernel,iterations = 1)
+
+    #detect blobs and get keypoints
+    keypoints = blob_detect.detect(gray_thresh)
+
+    #draw keypoints
+    img_w_keypoints = cv2.drawKeypoints(frame,keypoints,np.array([]),(0,0,255),cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
     #__END PLAIN THRESHOLD METHOD__
 
     #display the image
-    cv2.imshow('cardboard', gray_erode)
-    cv2.imshow('original', frame)
+    cv2.imshow('cardboard', img_w_keypoints)
+    cv2.imshow('original', gray_thresh)
 
     #quit
     if cv2.waitKey(67) & 0xFF == ord('q'):  #67 ms ~= 15 fps
